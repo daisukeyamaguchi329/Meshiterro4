f88eaaad3f660592e5866047aab4998528fc4a91
EC2 Default User <gaman55894@gmail.com>
2021-08-22 13:54:03 +0000

    Third Commit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '125a9e0475567a7b22bc01736e6f8157f1e85187a0a93eabd2719bba4e5e1d3e4d65f4658e514201cfd3e6a39ebb6132a78ed1efda7b505cf2b74fdf9d3a7d97'
